3fcb8966a44d8baf91e487910ba04a9f8f7c05cc
Gilles AHOUANTCHEDE <gilles@cyberspector.com>
2023-12-06 14:53:30 +0100

    feat: fix analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,9 +11,9 @@ import 'rc-drawer/assets/index.css';
 
 export default function CustomApp({ Component, pageProps }) {
   useEffect(() => {
-    initGA();
-    logPageView();
-    Router.events.on('routeChangeComplete', logPageView);
+    // initGA();
+    // logPageView();
+    // Router.events.on('routeChangeComplete', logPageView);
   }, []);
 
   return <Component {...pageProps} />;
